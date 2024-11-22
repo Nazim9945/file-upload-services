70510ff1af59e900d957cc711c4bdea437b13388
Nazim Saifi <saifinazim2311@gmail.com>
2024-11-22 17:52:28 +0530

    vediolimit is working fine

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,10 +3,13 @@ import {localfileupload} from '../controllers/localfileupload';
 import { imageupload } from '../controllers/imageupload';
 import { vedioupload } from '../controllers/vedioupload';
 import { imageReducerupload } from '../controllers/imageReduceupload';
+import { vedioReducerupload } from '../controllers/vedioReducerupload';
 const router=express.Router()
 
 router.post('/localfileupload', localfileupload);
 router.post('/imageupload', imageupload);
 router.post('/vedioupload', vedioupload);
 router.post('/imageReducerupload', imageReducerupload);
+router.post('/vedioReducerupload', vedioReducerupload);
+
 export default router
