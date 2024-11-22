94855e693ac6f0e1b34f4482309ffe6d1579b267
Nazim Saifi <saifinazim2311@gmail.com>
2024-11-22 17:38:22 +0530

    imageReducer workin fine

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,11 @@ import express from 'express'
 import {localfileupload} from '../controllers/localfileupload';
 import { imageupload } from '../controllers/imageupload';
 import { vedioupload } from '../controllers/vedioupload';
+import { imageReducerupload } from '../controllers/imageReduceupload';
 const router=express.Router()
 
 router.post('/localfileupload', localfileupload);
 router.post('/imageupload', imageupload);
 router.post('/vedioupload', vedioupload);
+router.post('/imageReducerupload', imageReducerupload);
 export default router
